c634326c0e5e21a799eb332e6458462955af85be
TheOddlyWolf <107967330+TheOddlyWolf@users.noreply.github.com>
2022-10-18 21:13:58 -0500

    Add files via upload

--- PROC36-1_4-actividad-maestra1-master/sketch.js
+++ PROC36-1_4-actividad-maestra1-master/sketch.js
@@ -1,5 +1,6 @@
 var Ball, database;
-var position;
+var position;position2;
+var Ball2;
 
 
 function setup(){
@@ -10,32 +11,56 @@ function setup(){
   Ball = createSprite(250,250,10,10);
   Ball.shapeColor = "red";
 
-  var BallPosition = database.ref('ball/positio');
+  Ball2 = createSprite(250,250,10,10);
+  Ball2.shapeColor = "Blue";
+
+  var BallPosition = database.ref('bola/posicion');
   BallPosition.on("value", readPosition, showError);
+
+  var BallPosition2 = database.ref('bola2/posicion2');
+  BallPosition2.on("value",readPosition2, showError);
 }                      
 
 function draw(){
   background("white");
   
     if(keyDown(LEFT_ARROW)){
-      writePosition(-1,0);
+      writePosition(-3,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-      writePosition(1,0);
+      writePosition(3,0);
     }
     else if(keyDown(UP_ARROW)){
-      writePosition(0,-1);
+      writePosition(0,-3);
     }
     else if(keyDown(DOWN_ARROW)){
-      writePosition(0,+1);
+      writePosition(0,+3);
     }
+
+   if(keyDown(65)){
+      writePosition2(-3,0);
+   }
+    else if(keyDown(68)){
+    writePosition2(3,0);
+    }
+    else if(keyDown(87)){
+      writePosition2(0,-3);
+   }
+    else if(keyDown(83)){
+    writePosition2(0,+3);
+   }
     drawSprites();
   
 }
 
 function writePosition(x,y){
-  database.ref('ball/positio').set({'x':position.x + x, 'y': position.y + y});
- 
+  database.ref('bola/posicion').set({'x':position.x + x, 'y':position.y + y});
+  
+}
+
+function writePosition2(x,y){
+  database.ref('bola2/posicion2').set({'x':position2.x + x, 'y':position2.y + y});
+
 }
 
 function readPosition(data){
@@ -43,7 +68,16 @@ function readPosition(data){
   console.log(position.x);
   Ball.x = position.x;
   Ball.y = position.y;
+
 }
+
+function readPosition2(data){
+  position2 = data.val();
+  console.log(position2.x);
+  Ball2.x = position2.x;
+  Ball2.y = position2.y;
+}
+
 
 function showError(){
   console.log("Error al escribir en la base de datos");
